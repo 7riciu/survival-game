dc8b8ea08ce721dae7aff0573c380df8f420bcf6
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-10-26 12:51:24 +0200

    I made a hungersystem

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
+@onready var player_health = 100
+@onready var player_hunger = 100
+@onready var player_thirst = 100
 var speed = 300.0
+
+func _ready() -> void:
+	hunger_decrese()
 
 func _physics_process(_delta: float) -> void:
 	
@@ -10,5 +16,14 @@ func _physics_process(_delta: float) -> void:
 	)
 	
 	velocity = direction * speed
-	
 	move_and_slide()
+
+func hunger_decrese():
+	while player_hunger > 0:
+		await get_tree().create_timer(2).timeout
+		player_hunger -= 2
+		print(player_hunger)
+	
+func hungry_health_decrease():
+	await get_tree().create_timer(1).timeout
+	player_health -= 2
