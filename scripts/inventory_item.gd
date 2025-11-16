24a5536430d6220be4c8dd32053e9e5e42317f24
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-11-16 16:01:28 +0200

    Created InventoryItem

--- scripts/inventory_item.gd
+++ scripts/inventory_item.gd
@@ -1,8 +1,31 @@
 extends Control
-class_name Item
+class_name InventoryItem
 
-var item_name: String
-var icon: Texture2D
+var amount: int = 0
 
-func _ready() -> void:
-	add_to_group("items")
+var sprite: Sprite2D
+var label: Label
+
+func set_data(_name: String, _icon: Texture2D, _is_stackable: bool, _amount: int):
+	self.item_name = name
+	self.name = _name
+	self.icon = _icon
+	self.is_stackble = _is_stackable
+	self.amount = _amount
+	
+func _process(_delta: float) -> void:
+	self.sprite.texture = self.icon
+	self.set_sprite_size_to(sprite, Vector2(42, 42))
+	if is_stackable:
+		self.label.text = str(self.amount)
+	else:
+		label.visible = false
+
+func set_sprite_size_to(sprite: Sprite2D, size: Vector2):
+	var texture_size = sprite.texture.get_size()
+	var scale_factor = Vector2(size.x / texture_size.x, size.y / texture_size.y)
+	sprite.scale = scale_factor
+	
+func fade():
+	self.sprite.modulate = Color(1, 1, 1, 0.4)
+	self.label.modulate = Color(1, 1, 1, 0.4)
