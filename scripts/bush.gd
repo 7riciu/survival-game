ca6083f0a7f08ab6261fe00721cf9ac980fab7ef
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-11-23 23:34:58 +0200

    Berries from the bush are added to the inventory

--- scripts/bush.gd
+++ scripts/bush.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 @onready var hunger_ui = get_tree().get_first_node_in_group("hunger_ui")
 @onready var berry_ui = get_tree().get_first_node_in_group("berry_ui")
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var berry = get_tree().get_first_node_in_group("berry")
 @onready var bush_area = $Area2D
 
 var berry_on_bush = 20
@@ -15,6 +16,7 @@ var has_berry = true
 func _process(_delta: float) -> void:
 	if bush_area.can_collect_berry and berry_on_bush > 0 and Input.is_action_just_pressed("e"):
 		berry_on_bush -= 10
+		player.pick_up_item(berry)
 		berry_ui.berry_collect()
 	elif berry_on_bush <= 0:
 		has_berry = false
