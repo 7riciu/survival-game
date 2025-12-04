1eb776d0ccf31af47972a2e9d3545187e0ccecc5
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-12-04 20:18:18 +0200

    Added a rock you can collect stone from :)

--- scripts/bush.gd
+++ scripts/bush.gd
@@ -1,11 +1,5 @@
 extends CharacterBody2D
 
-@export var item_name: String = ""
-@export var icon: Texture2D
-@export var is_stackable: bool = true
-
-@onready var hunger_ui = get_tree().get_first_node_in_group("hunger_ui")
-@onready var berry_ui = get_tree().get_first_node_in_group("berry_ui")
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var berry_scene: PackedScene = preload("res://scenes/berry.tscn")
 @onready var bush_area = $Area2D
@@ -17,7 +11,9 @@ func _process(_delta: float) -> void:
 	if bush_area.can_collect_berry and berry_on_bush > 0 and Input.is_action_just_pressed("e"):
 		berry_on_bush -= 10
 		var berry = berry_scene.instantiate()
-		berry.global_position = self.global_position + Vector2(0, 10)
+		berry.global_position = self.global_position + Vector2(0, 100)
 		get_tree().current_scene.add_child(berry)
 	elif berry_on_bush <= 0:
+		$AnimatedSprite2D.animation = "empty"
+		$AnimatedSprite2D.play()
 		has_berry = false
---
+++ scripts/rock.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var stone_scene: PackedScene = preload("res://scenes/stone.tscn")
+@onready var rock_area = $Area2D
+
+var stone_stock = 20
+var has_stone = true
+
+func _process(_delta: float) -> void:
+	if rock_area.can_collect_stone and stone_stock > 0 and Input.is_action_just_pressed("e"):
+		stone_stock -= 2
+		var stone = stone_scene.instantiate()
+		stone.global_position = self.global_position + Vector2(0, 90)
+		get_tree().current_scene.add_child(stone)
+	elif stone_stock <= 0:
+		has_stone = false
