00e0f8112590bb09437b802e5ba61cab36fba4ab
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2026-01-18 23:04:57 +0200

    Improved the crafting buttons and added functionality

--- crafting_recipes.gd
+++ crafting_recipes.gd
@@ -3,6 +3,8 @@ extends Node
 const RECIPES = {
 	"sword": {
 		"icon": preload("res://assets/pickaxe.png"),
+		"description": "sword",
+		"action": "buy_sword",
 		"requires": {
 			"wood": 10,
 			"stone": 10
@@ -11,6 +13,18 @@ const RECIPES = {
 	
 	"pickaxe": {
 		"icon": preload("res://assets/pickaxe.png"),
+		"description": "pickaxe",
+		"action": "buy_pickaxe",
+		"requires": {
+			"wood": 10,
+			"stone": 10
+		}
+	},
+	
+	"axe": {
+		"icon": preload("res://assets/pickaxe.png"),
+		"description": "axe",
+		"action": "buy_axe",
 		"requires": {
 			"wood": 10,
 			"stone": 10
--- scripts/crafting_item.gd
+++ scripts/crafting_item.gd
@@ -3,9 +3,18 @@ extends Button
 var item_id
 var recipe
 
-@onready var image = $TextureRect
-@onready var label = $Label
-
 func setup(id, data):
 	item_id = id
 	recipe = data
+	print(item_id)
+	
+	$TextureRect.texture = recipe.icon
+	$Label.text = recipe.description
+
+func _pressed() -> void:
+	match item_id:
+		"pickaxe":
+			buy_pickaxe()
+
+func buy_pickaxe():
+	print("pickaxe")
