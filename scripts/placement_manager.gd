684f7742b7e795eccc13b4963be5159a5fa54412
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2026-01-21 22:01:03 +0200

    fixed collision with the craftable items

--- scripts/placement_manager.gd
+++ scripts/placement_manager.gd
@@ -2,17 +2,21 @@ extends Node2D
 
 var can_drag = false
 var current_item: Node2D = null
+var coll_shape = null
 
 func _process(_delta: float) -> void:
 	if current_item:
 		if Input.is_action_just_pressed("e"):
 			can_drag = true
+			
 		if Input.is_action_just_pressed("r"):
 			can_drag = false
 		if can_drag:
 			current_item.position = get_global_mouse_position()
 		if Input.is_action_just_pressed("q"):
 			current_item.modulate.a = 1
+			coll_shape.disabled = false
+			coll_shape = null
 			current_item = null
 
 func start_placing(scene: PackedScene, pos: Vector2):
@@ -21,10 +25,8 @@ func start_placing(scene: PackedScene, pos: Vector2):
 	current_item = scene.instantiate()
 	add_child(current_item)
 	current_item.position = pos
+	coll_shape = current_item.get_node_or_null("CollisionShape2D")
+	if coll_shape:
+		coll_shape.disabled = true
 	current_item.modulate.a = 0.5
-	
-func confirm_placement():
-	if current_item:
-		current_item.modulate.a = 1
-		current_item = null
 	
