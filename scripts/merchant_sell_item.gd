d5fed7d8389c5031ac2fd9259f49847cff8f6608
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2026-01-25 12:02:22 +0200

    Fixed coin coin counting

--- scripts/merchant_sell_item.gd
+++ scripts/merchant_sell_item.gd
@@ -4,6 +4,6 @@ func setup(id):
 	var item_name = id.id
 	var item = load("res://items/" + str(item_name) + ".tres")
 	$Panel/TextureRect.texture = item.icon
-	$Panel/Amount.text = str(inventory_data.items[id])
 	$Panel/Name.text = item.display_name
-	$Panel/Price.text = str(item.sell_price)
+	$Panel/Amount.text = "Amount: " + str(inventory_data.items[id])
+	$Panel/Price.text = "Price: " + str(item.sell_price)
--- scripts/sell_button.gd
+++ scripts/sell_button.gd
@@ -7,6 +7,6 @@ func _on_pressed() -> void:
 	var item_name = $"../Name".text
 	var sold_item = load("res://items/" + str(item_name) + ".tres")
 	inventory_data.remove(sold_item, 1)
-	inventory_data.add(coin_item, 1)
+	inventory_data.add(coin_item, sold_item.sell_price)
 	get_tree().current_scene.get_node("UI").get_node("MerchantSellMenu").sell_clear()
 	get_tree().current_scene.get_node("UI").get_node("MerchantSellMenu").sell_items()
