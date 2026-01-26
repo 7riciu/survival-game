7f9943866dffffec7084bbd86f2d5a7f674d444d
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2026-01-26 23:47:39 +0200

    Added a deer npc that trades a spade with you

--- scripts/deer.gd
+++ scripts/deer.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-@onready var merchant_area = $Area2D
-var dialogue_scene = preload("res://scenes/merchant_dialogue.tscn")
+@onready var deer_area = $Area2D
+var dialogue_scene = preload("res://scenes/deer_dialogue.tscn")
 
 func _process(_delta: float) -> void:
-	if merchant_area.can_interact and Input.is_action_just_pressed("e"):
+	if deer_area.can_interact and Input.is_action_just_pressed("e"):
 		var dialogue = dialogue_scene.instantiate()
 		get_tree().current_scene.add_child(dialogue)
 		dialogue.start_dialogue()
