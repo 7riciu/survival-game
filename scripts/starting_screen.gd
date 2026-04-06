cf719e42232398dc7dfabbd83ef85972560661a2
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2026-04-06 20:43:06 +0300

    Added a little timer on starting and end screens

--- scripts/starting_screen.gd
+++ scripts/starting_screen.gd
@@ -1,5 +1,6 @@
 extends Control
 
 func _process(_delta: float) -> void:
+	await  get_tree().create_timer(1.0).timeout
 	if Input.is_action_just_pressed("click"):
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
